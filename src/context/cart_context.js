2c06dd9db2965324e0261f5931b225a62dcd9322
Rashadfrd <rashadnf@code.edu.az>
2023-03-01 02:49:58 +0400

    cart localstorage

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,21 +1,46 @@
-import React, { useContext, useReducer } from "react"
+import React, { useContext, useEffect, useReducer } from "react"
 import cartReducer from "../reducers/cart_reducer"
 
 
 export const CartContext = React.createContext()
 
+const getLocalStorageItems = () => {
+    let cart = localStorage.getItem('cart')
+    if(cart){
+        return JSON.parse(localStorage.getItem('cart'))
+    }
+    else{
+        return []
+    }
+}
+const getLocalStorageAmount = () => {
+    let amount = localStorage.getItem('totalAmount')
+    if(amount){
+        return JSON.parse(localStorage.getItem('totalAmount'))
+    }
+    else{
+        return 0
+    }
+}
 
 const CartProvider = ({children}) => {
 
     const[state,dispatch] = useReducer(cartReducer,{
-        cartItems:[],
-        totalAmount:0
+        cartItems: getLocalStorageItems(),
+        totalAmount:getLocalStorageAmount()
     })
 
+    
     const data = {
         state,
         dispatch
     }
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(state.cartItems))
+        localStorage.setItem('totalAmount', JSON.stringify(state.totalAmount))
+    },[state])
+
     return(
         <CartContext.Provider value={data}>
             {children}
