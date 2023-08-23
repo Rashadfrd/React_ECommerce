c92a6954e070f8a024a6fcc7b81bb39e61fd5d1b
Rashadfrd <rashadnf@code.edu.az>
2023-08-23 23:54:32 +0400

    deals component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,13 @@ import FeaturedProducts from '../components/FeaturedProducts'
 import HomeHero from '../components/HomeHero'
 import InfoCards from '../components/InfoCards'
 import Partners from '../components/Partners'
+import Deals from '../components/HomeDeals'
 
 const Home = () => {
   return (
     <div>
       <HomeHero />
+      <Deals />
       <FeaturedProducts />
       <InfoCards />
       <Partners />
