58aa9088ed70d0ad13b480ee47d9a32ee876a544
Rashadfrd <rashadnf@code.edu.az>
2023-08-24 23:18:25 +0400

    infocards update

--- src/components/InfoCards/index.js
+++ src/components/InfoCards/index.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import classes from './style.module.css'
-import { homeInfos } from '../../utils/constants'
+import React from "react";
+import classes from "./style.module.css";
+import { homeInfos } from "../../utils/constants";
 
 const InfoCards = () => {
   return (
-    <div className={classes.infoCard}>
-        {homeInfos.map((item)=>{
-            return(
-                <div key={item.id} className={classes.card}>
-                    <div className={classes.cardImg}>{item.icon}</div>
-                    <div className={classes.cardInfo}>
-                        <p className={classes.textTitle}>{item.name}</p> 
-                        <p className={classes.textBody}>{item.content}</p>
-                    </div>
-                </div>
-            )
-        })}
-    </div>
-  )
-}
+    <section className={classes.infoCard}>
+      {homeInfos.map((item, index) => {
+        return (
+        <>
+          <div className={classes.card}>
+            <div className={classes.cardLeft}>
+              {item.icon}
+            </div>
+            <div className={classes.cardInfo}>
+              <p className={classes.textTitle}>{item.name}</p>
+              <p className={classes.textBody}>{item.content}</p>
+            </div>
+          </div>
+          {
+            index !== homeInfos.length-1 &&
+            <div className={classes.line}></div>
+          }
+        </>
+        );
+      })}
+    </section>
+  );
+};
 
-export default InfoCards
+export default InfoCards;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,90 +1,98 @@
-import React from 'react'
-import { GiCompass, GiDiamondHard, GiStabbedNote } from 'react-icons/gi'
+import React from "react";
+import {AiOutlineShoppingCart, AiOutlineLock, AiOutlinePhone} from 'react-icons/ai'
+import {CiMoneyBill} from 'react-icons/ci'
 
 export const links = [
-    {
-      id: 1,
-      text: 'Home',
-      url: '/',
-    },
-    {
-      id: 2,
-      text: 'About',
-      url: '/about',
-    },
-    {
-      id: 3,
-      text: 'Products',
-      url: '/products',
-    },
-  ]
+  {
+    id: 1,
+    text: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "About",
+    url: "/about",
+  },
+  {
+    id: 3,
+    text: "Products",
+    url: "/products",
+  },
+];
 
-  export const homeInfos = [
-    {
-      id:1,
-      icon:<GiCompass size={45} />,
-      name:'Mission',
-      content:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi'
-    },
-    {
-      id:2,
-      icon:<GiDiamondHard size={45} />,
-      name:'Vision',
-      content:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi'
-    },
-    {
-      id:3,
-      icon:<GiStabbedNote size={45} />,
-      name:'History',
-      content:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi'
-    }
-  ]
-  
+export const homeInfos = [
+  {
+    id: 1,
+    icon: <AiOutlineShoppingCart size={33} />,
+    name: "Free Shipping",
+    content: "Orders Above $200",
+  },
+  {
+    id: 2,
+    icon: <AiOutlinePhone size={38} />,
+    name: "Money Back",
+    content:
+      "30 day guarantee",
+  },
+  {
+    id: 3,
+    icon: <CiMoneyBill size={38} />,
+    name: "Premium Support",
+    content:
+      "Phone and email support",
+  },
+  {
+    id: 4,
+    icon: <AiOutlineLock size={35} />,
+    name: "Secure Payments",
+    content:
+      "Secured by Stripe",
+  },
+];
 
-
-  export const partners = [
-    {
-      id:1,
-      src:'https://kontakt.az/wp-content/uploads/2020/03/Acer_png.webp'
-    },
-    {
-      id:2,
-      src:'https://kontakt.az/wp-content/uploads/2020/03/Beurer_png.webp'
-    },
-    {
-      id:3,
-      src:'https://kontakt.az/wp-content/uploads/2020/04/3_png.webp'
-    },
-    {
-      id:4,
-      src:'https://kontakt.az/wp-content/uploads/2020/03/TOYOTA_png.webp'
-    },
-    {
-      id:5,
-      src:'https://kontakt.az/wp-content/uploads/2020/03/Panasonic_png.webp'
-    },
-    {
-      id:6,
-      src:'https://kontakt.az/wp-content/uploads/2020/03/Kenwood_png.webp'
-    },
-    {
-      id:7,
-      src:'https://kontakt.az/wp-content/uploads/2020/04/4_png.webp'
-    },
-    {
-      id:8,
-      src:'https://kontakt.az/wp-content/uploads/2022/09/1280px-PlayStation_logo_and_wordmark.svg-1_png.webp'
-    },
-    {
-      id:9,
-      src:'https://kontakt.az/wp-content/uploads/2020/03/Samsung_png.webp'
-    },
-    {
-      id:10,
-      src:'https://kontakt.az/wp-content/uploads/2020/03/Bosh_png.webp'
-    },
-    {
-      id:11,
-      src:'https://kontakt.az/wp-content/uploads/2021/11/%D0%91%D0%B5%D0%B7-%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-1_png.webp'
-    },
-  ]
+export const partners = [
+  {
+    id: 1,
+    src: "https://kontakt.az/wp-content/uploads/2020/03/Acer_png.webp",
+  },
+  {
+    id: 2,
+    src: "https://kontakt.az/wp-content/uploads/2020/03/Beurer_png.webp",
+  },
+  {
+    id: 3,
+    src: "https://kontakt.az/wp-content/uploads/2020/04/3_png.webp",
+  },
+  {
+    id: 4,
+    src: "https://kontakt.az/wp-content/uploads/2020/03/TOYOTA_png.webp",
+  },
+  {
+    id: 5,
+    src: "https://kontakt.az/wp-content/uploads/2020/03/Panasonic_png.webp",
+  },
+  {
+    id: 6,
+    src: "https://kontakt.az/wp-content/uploads/2020/03/Kenwood_png.webp",
+  },
+  {
+    id: 7,
+    src: "https://kontakt.az/wp-content/uploads/2020/04/4_png.webp",
+  },
+  {
+    id: 8,
+    src: "https://kontakt.az/wp-content/uploads/2022/09/1280px-PlayStation_logo_and_wordmark.svg-1_png.webp",
+  },
+  {
+    id: 9,
+    src: "https://kontakt.az/wp-content/uploads/2020/03/Samsung_png.webp",
+  },
+  {
+    id: 10,
+    src: "https://kontakt.az/wp-content/uploads/2020/03/Bosh_png.webp",
+  },
+  {
+    id: 11,
+    src: "https://kontakt.az/wp-content/uploads/2021/11/%D0%91%D0%B5%D0%B7-%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-1_png.webp",
+  },
+];
