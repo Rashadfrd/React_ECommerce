b477a935a2bd829adf8b16ccdb88f75214ed64b3
Rashadfrd <rashadnf@code.edu.az>
2023-08-31 03:08:39 +0400

    product page renew

--- src/components/ProductsMenu/index.js
+++ src/components/ProductsMenu/index.js
@@ -1,57 +1,103 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import classes from './style.module.css'
-import { useProductsContext } from '../../context/products_context'
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./style.module.css";
+import { useProductsContext } from "../../context/products_context";
+import Skeleton from "../Skeleton";
 
 const ProductsMenu = () => {
-    const {state} = useProductsContext()
+  const { state } = useProductsContext();
   return (
-    <section className={state.isProdSidebarOpen ? classes.menu : classes.menuWide}>
+    <section
+      className={state.isProdSidebarOpen ? classes.menu : classes.menuWide}
+    >
       <div className={classes.menuHeader}>
-        <div className={classes.productsCount}> {state.filteredProds.length} Products Found</div>
+        <div className={classes.productsCount}>
+          {" "}
+          {state.filteredProds.length} Products Found
+        </div>
         <div>
-            <form>
-                <label>Sort By</label>        
-            </form>
+          <form>
+            <label>Sort By</label>
+          </form>
         </div>
       </div>
       <div className={classes.products}>
-        {state.isLoading 
-        ?
-         'Sorry, no products matched your search'
-         :
-          state.filteredProds.map((prod)=>{
+        {state.isLoading &&
+          Array(6)
+            .fill(null)
+            .map((index) => {
+              return (
+                <div className={classes.productItemContainer} key={index+ Math.random()}>
+                  <div className={classes.productItem}>
+                    <div className={classes.prodImg}>
+                      <Skeleton className={classes.prodImgItem} />
+                    </div>
+                    <div className={classes.prodName}>
+                      <Skeleton width={100} height={20} />
+                    </div>
+                    <div className={classes.prodPrice}>
+                      <span className={classes.newPrice}>
+                        <Skeleton width={50} height={20} />
+                      </span>
+                      <span className={classes.oldPrice}>
+                        <Skeleton width={50} height={20} />
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        {state &&
+          state.filteredProds.map((prod) => {
             return (
-              <div className={classes.productItemContainer} key={prod.id} >
+              <div className={classes.productItemContainer} key={prod.id}>
                 <div className={classes.productItem}>
-                <div className={classes.prodImg}>
-                  <img className={classes.prodImgItem} src={prod.image} alt="" />
-                </div>
-                <div className={classes.prodName}>
-                {prod.name.charAt(0).toUpperCase() + prod.name.slice(1)}
-                </div>
-                <div className={classes.prodPrice}>
-                  <span className={classes.newPrice}>$ {prod.price.toFixed(2)}</span>
-                  <span className={classes.oldPrice}>$ {prod.price.toFixed(2)}</span>
+                  <Link to={prod.id} className={classes.prodImg}>
+                  <div className={classes.prodActionVertical}>
+                    <button className={classes.addToBasket}>
+                      <svg
+                        stroke="currentColor"
+                        fill="currentColor"
+                        strokeWidth="0"
+                        viewBox="0 0 16 16"
+                        height="22px"
+                        width="22px"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M8 1a2 2 0 0 1 2 2v2H6V3a2 2 0 0 1 2-2zm3 4V3a3 3 0 1 0-6 0v2H3.36a1.5 1.5 0 0 0-1.483 1.277L.85 13.13A2.5 2.5 0 0 0 3.322 16h9.355a2.5 2.5 0 0 0 2.473-2.87l-1.028-6.853A1.5 1.5 0 0 0 12.64 5H11zm-1 1v1.5a.5.5 0 0 0 1 0V6h1.639a.5.5 0 0 1 .494.426l1.028 6.851A1.5 1.5 0 0 1 12.678 15H3.322a1.5 1.5 0 0 1-1.483-1.723l1.028-6.851A.5.5 0 0 1 3.36 6H5v1.5a.5.5 0 1 0 1 0V6h4z"></path>
+                      </svg>
+                    </button>
+                    <button className={classes.quickView}>.</button>
+                  </div>
+                  <div className={classes.prodLink}>
+                    <button className={classes.prodLinkBtn}>
+                      View Product
+                    </button>
+                  </div>
+                    <img
+                      className={classes.prodImgItem}
+                      src={prod.image}
+                      alt=""
+                    />
+                  </Link>
+                  <div className={classes.prodName}>
+                    {prod.name.charAt(0).toUpperCase() + prod.name.slice(1)}
+                  </div>
+                  <div className={classes.prodPrice}>
+                    <span className={classes.newPrice}>
+                      $ {prod.price.toFixed(2)}
+                    </span>
+                    <span className={classes.oldPrice}>
+                      $ {prod.price.toFixed(2)}
+                    </span>
+                  </div>
                 </div>
               </div>
-              </div>
-                // <div key={prod.id} className={classes.productWrapper}>
-                //          <div className={classes.wrapperItem}>
-                //     <Link to={prod.id}>
-                //             <img src={prod.image} style={{borderRadius:'5px',boxShadow:'rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px' }} width='100%' height='230px' alt="" />
-                //     </Link>
-                //             <div className={classes.itemInfos}>
-                //                 <span>{prod.name.charAt(0).toUpperCase() + prod.name.slice(1)}</span>
-                //                 <span>$ {prod.price.toFixed(2)}</span>
-                //             </div>
-                //         </div>
-                // </div> 
-                    )
-                })}
+            );
+          })}
       </div>
     </section>
-  )
-}
-    
-export default ProductsMenu
+  );
+};
+
+export default ProductsMenu;
--- src/components/ProductsSidebar/index.js
+++ src/components/ProductsSidebar/index.js
@@ -1,24 +1,113 @@
-import React from 'react'
-import classes from './style.module.css'
-import { useProductsContext } from '../../context/products_context'
+import React from "react";
+import classes from "./style.module.css";
+import { useProductsContext } from "../../context/products_context";
+import Skeleton from "../Skeleton";
 
 const ProductsSidebar = () => {
-  const{state,dispatch} = useProductsContext()
+  const { state, dispatch } = useProductsContext();
 
   return (
-    <aside className={state.isProdSidebarOpen ? classes.prodSidebar : `${classes.clicked}`}>
+    <aside
+      className={
+        state.isProdSidebarOpen ? classes.prodSidebar : `${classes.clicked}`
+      }
+    >
       <div className={classes.prodSidebarHeader}>
-        <button className={classes.prodSidebarFilter} onClick={()=>{dispatch({type:'PRODSIDEBAR_ACTIVE',val:false})}} >Filter</button>
+        <button
+          className={classes.prodSidebarFilter}
+          onClick={() => {
+            dispatch({ type: "PRODSIDEBAR_ACTIVE", val: false });
+          }}
+        >
+          Filter
+        </button>
         <button className={classes.prodSidebarClean}>Clean All</button>
       </div>
-      <div className={classes.formItem}>
-          <h5>Category</h5>
-          { state.categories.map((item,index)=>{
-            return <button onClick={(e)=>{dispatch({type:'CATEGORY_FILTER',payload:e.target.textContent})}} className={classes.categoryBtn} key={index} type='button'>{item.toUpperCase().substring(0,1) + item.slice(1)}</button>
-          })}
+      <div className={classes.prodSidebarBottom}>
+        {state.isLoading && (
+          <>
+            <div className={classes.formItem}>
+              <h4><Skeleton width={100} height={30} /></h4>
+              {Array(6).fill(null).map((item, index) => {
+                return (
+                  <button
+                    className={classes.categoryBtn}
+                    key={index}
+                  >
+                    <Skeleton width={80} height={20} />
+                  </button>
+                );
+              })}
+            </div>
+          </>
+        )}
+        {!state.isLoading && (
+          <>
+            <div className={classes.formItem}>
+              <h4>Category</h4>
+              {state.categories.map((item, index) => {
+                return (
+                  <button
+                    onClick={(e) => {
+                      dispatch({
+                        type: "CATEGORY_FILTER",
+                        payload: e.target.textContent,
+                      });
+                    }}
+                    className={classes.categoryBtn}
+                    key={index}
+                    type="button"
+                  >
+                    {item.toUpperCase().substring(0, 1) + item.slice(1)}
+                  </button>
+                );
+              })}
+            </div>
+            <div className={classes.formItem}>
+              <h4>Category</h4>
+              {state.categories.map((item, index) => {
+                return (
+                  <button
+                    onClick={(e) => {
+                      dispatch({
+                        type: "CATEGORY_FILTER",
+                        payload: e.target.textContent,
+                      });
+                    }}
+                    className={classes.categoryBtn}
+                    key={index}
+                    type="button"
+                  >
+                    {item.toUpperCase().substring(0, 1) + item.slice(1)}
+                  </button>
+                );
+              })}
+            </div>
+            <div className={classes.formItem}>
+              <h4>Category</h4>
+              {state.categories.map((item, index) => {
+                return (
+                  <button
+                    onClick={(e) => {
+                      dispatch({
+                        type: "CATEGORY_FILTER",
+                        payload: e.target.textContent,
+                      });
+                    }}
+                    className={classes.categoryBtn}
+                    key={index}
+                    type="button"
+                  >
+                    {item.toUpperCase().substring(0, 1) + item.slice(1)}
+                  </button>
+                );
+              })}
+            </div>
+          </>
+        )}
       </div>
     </aside>
-  )
-}
+  );
+};
 
-export default ProductsSidebar
+export default ProductsSidebar;
